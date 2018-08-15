ec197f2b97ba3bd23a6d7d31dc866f910eaa3749
yukihirop <te108186@gmail.com>
2018-08-15 21:20:02 +0900

    Modify API return type

--- fcoin/api.go
+++ fcoin/api.go
@@ -13,13 +13,13 @@ type API interface {
 	Endpoint
 }
 
-func NewAPI(opts ...Option) API {
+func NewAPI(opts ...Option) (c *Configure) {
 	// https://stackoverflow.com/questions/44543374/cannot-take-the-address-of-and-cannot-call-pointer-method-on?noredirect=1&lq=1
-	c := (&Configure{}).setDefault()
+	c = (&Configure{}).setDefault()
 	for _, opt := range opts {
 		opt(c)
 	}
-	return c
+	return
 }
 
 // getter
