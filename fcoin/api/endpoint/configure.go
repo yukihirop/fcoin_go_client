7752be268bb9d8a41184049983e9b6e6684e56f3
yukihirop <te108186@gmail.com>
2018-08-17 13:43:49 +0900

    Implement apiConfig

--- fcoin/api/endpoint/configure.go
+++ fcoin/api/endpoint/configure.go
@@ -7,6 +7,13 @@ import (
 
 type EndpointConfigure struct {
 	*api.APIConfigure
+	//  The adapter that will be used to connect if none is set
+	Adapter   string
+	Endpoint  string
+	UserAgent string
+	Proxy     string
+	ApiKey    string
+	SecretKey string
 }
 
 type Endpoint struct {
@@ -105,4 +112,15 @@ func setEndpoint(opts EndpointsOptions) (ma *Endpoint) {
 		opt(ma)
 	}
 	return
+}
+
+func apiConfig(c *EndpointConfigure) *(api.APIConfigure) {
+	var config api.APIConfigure
+	config.Adapter = c.Adapter
+	config.Endpoint = c.Endpoint
+	config.UserAgent = c.UserAgent
+	config.Proxy = c.Proxy
+	config.ApiKey = c.ApiKey
+	config.SecretKey = c.SecretKey
+	return &config
 }
