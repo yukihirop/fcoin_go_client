2f3301a497a86e73d499f24f238adbb9e3c3fac9
yukihirop <te108186@gmail.com>
2018-08-20 01:28:08 +0900

    Add WssEndpoint into Configure

--- fcoin/configure.go
+++ fcoin/configure.go
@@ -4,6 +4,7 @@ import "fcoin_go_client/fcoin/api/endpoint"
 
 const DEFAULT_ADAPTER = "adapter"
 const DEFAULT_ENDPOINT = "https://api.fcoin.com/v2/"
+const DEFAULT_WSS_ENDPOINT = "wss://api.fcoin.com/v2/ws"
 const DEFAULT_USER_AGENT = "Fcoin Go Client"
 const DEFAULT_PROXY = ""
 const DEFAULT_API_KEY = "fcoin public api key"
@@ -11,17 +12,19 @@ const DEFAULT_SERECT_KEY = "fcoin secret api key"
 
 type Configure struct {
 	//  The adapter that will be used to connect if none is set
-	Adapter   string
-	Endpoint  string
-	UserAgent string
-	Proxy     string
-	ApiKey    string
-	SecretKey string
+	Adapter     string
+	Endpoint    string
+	WssEndpoint string
+	UserAgent   string
+	Proxy       string
+	ApiKey      string
+	SecretKey   string
 }
 
 //Option is function value of functional options
 // http://text.baldanders.info/golang/functional-options-pattern/
 type Option func(*Configure)
+type Options []Option
 
 func Adapter(a string) Option {
 	return func(c *Configure) {
@@ -32,6 +35,12 @@ func Adapter(a string) Option {
 func EndPoint(e string) Option {
 	return func(c *Configure) {
 		c.Endpoint = e
+	}
+}
+
+func WssEndpoint(we string) Option {
+	return func(c *Configure) {
+		c.WssEndpoint = we
 	}
 }
 
@@ -65,12 +74,13 @@ func (c *Configure) setConfig(config *Configure) *Configure {
 
 func (c *Configure) setDefault() *Configure {
 	c = &Configure{
-		Adapter:   DEFAULT_ADAPTER,
-		Endpoint:  DEFAULT_ENDPOINT,
-		UserAgent: DEFAULT_USER_AGENT,
-		Proxy:     DEFAULT_PROXY,
-		ApiKey:    DEFAULT_API_KEY,
-		SecretKey: DEFAULT_SERECT_KEY,
+		Adapter:     DEFAULT_ADAPTER,
+		Endpoint:    DEFAULT_ENDPOINT,
+		WssEndpoint: DEFAULT_WSS_ENDPOINT,
+		UserAgent:   DEFAULT_USER_AGENT,
+		Proxy:       DEFAULT_PROXY,
+		ApiKey:      DEFAULT_API_KEY,
+		SecretKey:   DEFAULT_SERECT_KEY,
 	}
 	return c
 }
