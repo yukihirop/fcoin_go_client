1c811e6ee93a46903e2e2598640542f14d64a9f3
yukihirop <te108186@gmail.com>
2018-08-28 00:42:51 +0900

    ginkgo bootstrap

--- fcoin/api/endpoint/endpoint_suite_test.go
+++ fcoin/api/endpoint/endpoint_suite_test.go
@@ -1,11 +1,24 @@
 package endpoint_test
 
 import (
+	"fcoin_go_client/fcoin/config"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/spf13/viper"
 )
+
+var fixedViper = viper.New()
+var customViper = viper.New()
+var VSetting = new(config.ValidationSetting)
+
+func SetValidationSetting() {
+	VSetting.FixedViper = fixedViper
+	VSetting.CustomViper = customViper
+	VSetting.CustomSettingPath = nil
+	VSetting.SetValidationSetting()
+}
 
 func TestEndpoint(t *testing.T) {
 	RegisterFailHandler(Fail)
