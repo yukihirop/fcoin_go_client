30833d0c4a17c651e0cc945bb57262a4d46fe0d4
yukihirop <te108186@gmail.com>
2018-08-27 23:45:51 +0900

    ginkgo bootstrap

---
+++ fcoin/api/endpoint/validator/validator_suite_test.go
@@ -0,0 +1,26 @@
+package validator_test
+
+import (
+	"fcoin_go_client/fcoin/config"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/spf13/viper"
+)
+
+var fixedViper = viper.New()
+var customViper = viper.New()
+var VSetting = new(config.ValidationSetting)
+
+func SetValidationSetting() {
+	VSetting.FixedViper = fixedViper
+	VSetting.CustomViper = customViper
+	VSetting.CustomSettingPath = nil
+	VSetting.SetValidationSetting()
+}
+
+func TestValidator(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Validator Suite")
+}
